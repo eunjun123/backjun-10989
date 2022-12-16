afa887e8a0c71475a12e70d1680a70b8fb33b23a
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 19:14:33 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var reader = new System.IO.StreamReader(Console.OpenStandardInput());
+            using var print = new System.IO.StreamWriter(Console.OpenStandardOutput());
+            int count = Convert.ToInt32(reader.ReadLine());
+            int[] list = new int[10001];
+
+            for (int i = 0; i < count; i++)
+            {
+                list[int.Parse(reader.ReadLine())]++;
+            }
+            for (int i = 0; i <= 10000; i++)
+            {
+                if (list[i] != 0)
+                {
+                    for (int j = 0; j < list[i]; j++)
+                    {
+                        print.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
